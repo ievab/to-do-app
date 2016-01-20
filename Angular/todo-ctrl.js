3a47eaea5042b942e958192613f373e16940908a
unknown <Ieva.Bumbiere@LV-ENS-WS-0027.internal.visma.com>
2016-01-20 18:30:07 +0200

    Task pievienošanas funkcijas

--- Angular/todo-ctrl.js
+++ Angular/todo-ctrl.js
@@ -7,7 +7,25 @@
     angular.module("ToDoApp")
 
         .controller("ToDoCtrl", function($scope) {
-            $scope.task = "1.Uzdevums";
+
+            var ToDoCtrl = this;
+
+            ToDoCtrl.tasks = [];
+            ;
+            ToDoCtrl.task = "";
+
+            ToDoCtrl.addTask = function() {
+              ToDoCtrl.tasks.push(ToDoCtrl.task);
+                ToDoCtrl.task = "";
+            };
+
+            ToDoCtrl.remove = function(task) {
+                ToDoCtrl.tasks.splice(task, 1);
+            };
+
+            ToDoCtrl.move = function() {
+
+            };
     });
 
 })();
