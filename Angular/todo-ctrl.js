b608470ec35d48fa9f41c7eb86a5daaf5a20827d
Ieva Bumbiere <Ieva Bumbiere>
2016-01-21 15:04:44 +0200

    Mob 21,01,2016

--- Angular/todo-ctrl.js
+++ Angular/todo-ctrl.js
@@ -7,7 +7,64 @@
     angular.module("ToDoApp")
 
         .controller("ToDoCtrl",["ToDoService", function(ToDoService) {
-        // kontroliera funkcijas un objekti
+            // kontroliera funkcijas un objekti
+            var taskController = this;
+            taskController.tasks = {};
+            taskController.task = '';
+
+            taskController.addTask = function(){
+                ToDoService.addTask(taskController.task).then(
+                    function (response) {
+                        taskController.getList();
+                        taskController.task = '';
+                    },
+                    function (data) {
+                        alert("Nevar nolasīt datus");
+                    })
+            };
+
+            taskController.deleteTask = function(id){
+                ToDoService.deleteTask(id).then(
+                    function (response) {
+                        taskController.getList();
+                    },
+                    function (data) {
+                        alert("Nevar nolasīt datus");
+                    })
+            };
+
+            taskController.doneTask = function(id){
+                ToDoService.doneTask(id).then(
+                    function (response) {
+                        taskController.getList();
+                    },
+                    function (data) {
+                        alert("Nevar nolasīt datus");
+                    })
+            };
+
+            taskController.undoneTask = function(id){
+                ToDoService.undoneTask(id).then(
+                    function (response) {
+                        taskController.getList();
+                    },
+                    function (data) {
+                        alert("Nevar nolasīt datus");
+                    })
+            };
+
+            taskController.getList = function(){
+                ToDoService.getTasks().then(
+                    function (response) {
+                        taskController.tasks = response;
+                    },
+                    function (data) {
+                        alert("Nevar nolasīt datus");
+                    }
+                );
+            };
+            taskController.getList();
+
     }]);
 
 })();
--- Angular/todoService.js
+++ Angular/todoService.js
@@ -6,13 +6,82 @@
 
     angular.module("ToDoApp")
 
-        .factory("ToDoService", function(){
+        .factory("ToDoService", ['$http', '$q', function($http, $q){
             var ToDoServicefactory = {};
-
+            var toDoMsLocation = "http://10.11.56.240:9969/api/todo/";
             //servisa funkcijas
+            var _getTasks = function() {
+                var deferred = $q.defer();
+                $http.get(toDoMsLocation).then(
+                    function (response) {
+                        deferred.resolve(response.data);
+                    },
+                    function (data) {
+                        deferred.reject(data);
+                    }
+                );
+                return deferred.promise;
+            };
 
+            var _saveTask = function(text) {
+                var deferred = $q.defer();
+                $http.post(toDoMsLocation, '{"task":"' + text +'"}').then(
+                    function (response) {
+                        deferred.resolve(response.data);
+                    },
+                    function (data) {
+                        deferred.reject(data);
+                    }
+                );
+                return deferred.promise;
+            };
+
+            var _deleteTask = function(id) {
+                var deferred = $q.defer();
+                $http.delete(toDoMsLocation + id).then(
+                    function (response) {
+                        deferred.resolve(response.data);
+                    },
+                    function (data) {
+                        deferred.reject(data);
+                    }
+                );
+                return deferred.promise;
+            };
+
+            var _doneTask = function(id) {
+                var deferred = $q.defer();
+                $http.put(toDoMsLocation + id + '/done').then(
+                    function (response) {
+                        deferred.resolve(response.data);
+                    },
+                    function (data) {
+                        deferred.reject(data);
+                    }
+                );
+                return deferred.promise;
+            };
+
+            var _undoneTask = function(id) {
+                var deferred = $q.defer();
+                $http.put(toDoMsLocation + id + '/undone').then(
+                    function (response) {
+                        deferred.resolve(response.data);
+                    },
+                    function (data) {
+                        deferred.reject(data);
+                    }
+                );
+                return deferred.promise;
+            };
+
+            ToDoServicefactory.getTasks = _getTasks;
+            ToDoServicefactory.addTask = _saveTask;
+            ToDoServicefactory.deleteTask = _deleteTask;
+            ToDoServicefactory.doneTask = _doneTask;
+            ToDoServicefactory.undoneTask = _undoneTask;
             return ToDoServicefactory;
 
-    });
+    }]);
 
 })();
